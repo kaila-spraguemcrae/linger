3eddf55af3c07a4b705a82fdb799f18e59864c20
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-03-05 11:37:40 -0800

    add environment in App and create lights compenent

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
-
+import React, { Suspense } from 'react';
+import { Canvas } from 'react-three-fiber';
+import { Environment, Sphere } from '@react-three/drei';
 
 function App() {
   return (
-    <>
-    </>
+    <Canvas camera= {{position: [0, 0, 20], fov: 50}}>
+      <Sphere/>
+      <Suspense fallback={null}>
+        <Environment 
+        files='https://rawcdn.githack.com/emmelleppi/component-material/9c967a77c15c84ee5c0be417c4440f6bdcebdb2f/example/studio_small_04_1k.hdr' 
+        path='' />
+      </Suspense>
+    </Canvas>
   );
 }
 
