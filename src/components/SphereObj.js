1d350e63aeeac816fcbecb3279d1777264d966b3
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-03-05 16:14:13 -0800

    add glsl for material.vert.head

--- src/components/SphereObj.js
+++ src/components/SphereObj.js
@@ -26,6 +26,37 @@ function SphereObj({ radius = 4 }){
           }
         }}
       > 
+        <Material.Vert.Head>
+          {
+            glsl`
+            #pragma glslify: snoise = require(glsl-noise-simplex/3d.glsl)
+            float fsnoise(float val1, float val2, float val3){
+              return snoise(vec3(val1,val2,val3));
+            }
+
+            vec3 distortFunct(vec3 transformed, float factor) {
+              float radiusVariation = -fsnoise
+              (
+                transformed.x * radiusNoiseFrequency + time, 
+                transformed.y * radiusNoiseFrequency + time,
+                radiusNoiseFrequency + time, 
+              ) * radiusVariationAmplitude * factor;
+              return normalize(transformed) * (radiusVariation + radius);
+            }
+
+            vec3 orthogonal(vec3 v) {
+              return normalize(abs(v.x) > abs (v.z) ? vec3(-v.y, v.y));
+            }
+
+            vec3 distortNormal(vec3 position, vec3 distortedPosition, vec3 normal){
+              vec3 tangent1 = orthogonal(normal);
+              vec tangent2 = normalize(cross(normal, tangent1));
+              vec nearby1 = position + tangent1 + 0.1;
+              return normalize(cross(distored1 - distortedPosition, distorted2 - distortedPosition));
+            }
+          `
+          }
+        </Material.Vert.Head>
         
       </Material>
     </Sphere>
