041d86455ac91f98fa4587704fdb73d7f5c1feac
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-03-08 11:01:16 -0800

    add rayleigh to tweaks

--- src/components/CustomSky.js
+++ src/components/CustomSky.js
@@ -7,15 +7,17 @@ import { useFrame } from 'react-three-fiber';
 function CustomSky() {
   useFrame(({ clock }) => (material.current.time = clock.getElapsedTime()));
   const  material = useRef();
-  const { inclination, azimuth  } = useTweaks({
+  const { inclination, azimuth, rayleigh  } = useTweaks({
     inclination:{ value: 0, min: 0, max: 1},
-    azimuth:{ value: 0.25, min: 0, max: 1}
+    azimuth:{ value: 0.25, min: 0, max: 1},
+    rayleigh: {value: 0.5, min: 0, max: 1}
   });
   return (<Sky
     ref = {material}
     distance={45000}
     inclination={inclination}
     azimuth={azimuth}
+    rayleigh={rayleigh}
   />);
 }
 
