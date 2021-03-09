0f632f33cec7e5dfda9219e11a3d4b979819c37a
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-03-09 14:09:31 -0800

    clean Scene

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,27 +1,10 @@
-import React, { useRef } from 'react'
-import {Canvas, useThree, useFrame } from 'react-three-fiber'
+import React from 'react'
+import { Canvas } from 'react-three-fiber'
 import { OrbitControls } from '@react-three/drei';
 import SphereObj from './SphereObj';
 import CustomSky from './CustomSky';
 
-export default function Scene(props) {
-
-  // const [mainLightColor, setMainLightColorState] = useState(() => {
-  //   return "rgb(183,136,101)"
-  // })
-
-
-  // const lightColorHandler = () => {
-  //   let r = Math.floor(Math.random() * 255)
-  //   let g = Math.floor(Math.random() * 255)
-  //   let b = Math.floor(Math.random() * 255)
-    
-  //   setMainLightColorState(() => {
-  //     return `rgb(${r},${g},${b})`
-  //     }
-  //   )
-  // }
-    
+export default function Scene() {
   
   return (
     <Canvas camera= {{position: [-15, 20, 30], near: 0.1, fov: 60}} >
