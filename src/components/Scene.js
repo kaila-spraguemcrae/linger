f232c500b835a0e677677686c82cf66caaad0508
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-03-10 15:50:28 -0800

    update camera

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { Canvas } from 'react-three-fiber'
-import { OrbitControls, softShadows } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 import SphereObj from './SphereObj';
 import CustomSky from './CustomSky';
 import Welcome from './Welcome';
 
 export default function Scene() {
-  // softShadows()
 
   return (
-    <Canvas shadowMap colorManagement camera= {{position: [-15, 20, 30], near: 0.1, fov: 60}} >
+    <Canvas shadowMap colorManagement camera= {{position: [-15, 20, 30], near: 0.1, fov: 60 }} >
       <Welcome/>
       <CustomSky/>
       <SphereObj /> 
