8ffb4125facd50b2ccd4a64d4df421386b8d5b99
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-03-09 16:35:00 -0800

    remove light from scene

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -9,18 +9,6 @@ export default function Scene() {
 
   return (
     <Canvas shadowMap colorManagement camera= {{position: [-15, 20, 30], near: 0.1, fov: 60}} >
-      {/* <directionalLight 
-          castShadow
-          position={[0,10,0]}
-          intensity={1.5}
-          shadow-mapSize-width={1024}
-          shadow-mapSize-height={1024}
-          shadow-camera-far={50}
-          shadow-camera-left={-10}
-          shadow-camera-right={10}
-          shadow-camera-top={10}
-          shadow-camera-bottom={-10}
-        /> */}
       <CustomSky/>
       <SphereObj /> 
       <OrbitControls/>
